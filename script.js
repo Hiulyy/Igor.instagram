e2dcd56482617f24712248b2a98457f786749c3e
Hiulyy <hiuly.github@gmail.com>
2025-02-07 11:46:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const botaoCadastro = document.querySelector(".botao-2");
+    
+//     botaoCadastro.addEventListener("click", function (event) {
+//         event.preventDefault(); 
+        
+//         const celularOuEmail = document.getElementById("celular-ou-email").value.trim();
+//         const senha = document.getElementById("senha").value.trim();
+//         const nomeCompleto = document.getElementById("nome-completo").value.trim();
+//         const nomeUsuario = document.getElementById("nome-de-usuario").value.trim();
+        
+//         if (celularOuEmail === "" || senha === "" || nomeCompleto === "" || nomeUsuario === "") {
+//             alert("Por favor, preencha todos os campos antes de continuar.");
+//         } else {
+//             window.location.href = "boas-vindas.html"; 
+//         }
+//     });
+// });
