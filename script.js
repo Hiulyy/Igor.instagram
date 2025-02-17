a01b7170642d7329ae3f243da01f7d9692a01979
Hiulyy <hiuly.github@gmail.com>
2025-02-17 12:45:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const botaoCadastro = document.querySelector(".botao-2");
-    
-//     botaoCadastro.addEventListener("click", function (event) {
-//         event.preventDefault(); 
+const modoEscuro = () =>{
+    const header =document.querySelector("header");
+    const footer =document.querySelector("footer"); 
+    const main = document.querySelector("main");
+    const butao = document.querySelector("button");
+
+    if(butao.classList.contains("modo-claro")){
+      header.classList.add("modo-escuro")
+      main.classList.add("modo-escuro")
+      footer.classList.add("modo-escuro")
+      butao.classList.add("modo-escuro")
+      butao.classList.remove("modo-claro")
+
+      butao.textContent = "Desativar modo escuro"
+
+    } else{
+        header.classList.remove("modo-escuro")
+        main.classList.remove("modo-escuro")
+        footer.classList.remove("modo-escuro")
+        butao.classList.add("modo-claro")
+
         
-//         const celularOuEmail = document.getElementById("celular-ou-email").value.trim();
-//         const senha = document.getElementById("senha").value.trim();
-//         const nomeCompleto = document.getElementById("nome-completo").value.trim();
-//         const nomeUsuario = document.getElementById("nome-de-usuario").value.trim();
-        
-//         if (celularOuEmail === "" || senha === "" || nomeCompleto === "" || nomeUsuario === "") {
-//             alert("Por favor, preencha todos os campos antes de continuar.");
-//         } else {
-//             window.location.href = "boas-vindas.html"; 
-//         }
-//     });
-// });
+      butao.textContent = "Ativar modo escuro"
+
+    }
+}
